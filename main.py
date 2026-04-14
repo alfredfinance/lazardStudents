f5e49c4017ad572560358b990bef86f42728ec88
alfredfinance <alfredfinance1per@gmail.com>
2026-04-14 11:15:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from zoneinfo import ZoneInfo
 # SELENIUM OPTIONS
 # ─────────────────────────────────────────────
 options = Options()
-# options.add_argument("--headless=new")
+options.add_argument("--headless=new")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
